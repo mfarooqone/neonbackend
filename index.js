b238814d75f16809dae63087a4137029baad5d5d
Muhammad Farooq Iqbal <mfarooqiqbal143@gmail.com>
2024-03-25 22:50:51 +0500

    WIP update port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cartRoute = require("./routes/cart");
 const { default: mongoose } = require("mongoose");
 
 /// INIT
-const PORT = 5001;
+const PORT = 5050;
 const app = express();
 const DB =
   "mongodb+srv://farooqone:iphonepakistaN@cluster0.yehdinj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
@@ -33,6 +33,6 @@ mongoose
     console.log(e);
   });
 
-  app.listen(PORT, "0.0.0.0", () => {
+  app.listen(PORT, "66.42.49.235", () => {
     console.log(`connected at port ${PORT}`);
   });
